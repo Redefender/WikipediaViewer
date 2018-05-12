1f736a03f86718d5bac38c64d62103f8c8a62b3b
Ezra Jesalva <ezra.jesalva15@gmail.com>
2018-05-11 22:14:18 -0400

    Change input styling. Change data entry into view (using element nodes). Change Add tooltip. Allow article link on click.

--- index.js
+++ index.js
@@ -12,12 +12,22 @@ $(".container").first().on("click",function(){
   $("form").parent().show();
 })
 
+let question = document.querySelector("#question");
+question.addEventListener("hover",function(){
+
+});
+console.log(question);
+
 var query = 'https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&format=json&list=search&origin=*&srsearch=';
 
 (function(){
 
   let form = document.querySelector("#submit");
   let input = document.querySelector("#query");
+
+  input.addEventListener("focus",function(e){
+    e.preventDefault();
+  });
 
 
   form.addEventListener("click",function(e){
@@ -29,56 +39,54 @@ var query = 'https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=ur
       input.placeholder = input.value;
 
       return query+input.value;
-    }
-
-
+    };
 
     $.get(insertParam(input.value),function(data){
-      console.log(data);
-      // var pageNum = Object.keys(data["query"]["pages"])[0]; //page key uknown, manually get (first key within pages)
+
       let searcher = data.query.search;
-      console.log(searcher);
-      // var title = data["query"]["pages"][pageNum].title;
-      // var url = data["query"]["pages"][pageNum].fullurl;
-
       let returnString = '';
-      returnString+= 'numpages: ' + searcher.length;
 
-      //put them in neat objects;
-      function page(title, wordcount,snippet){
-        this.title = title;
-        this.wordcount = wordcount;
-        this.snippet = snippet;
-      }
+
       for(i=0;i<searcher.length;i++){
-        returnString+='<div class="page">';
-        returnString+= '<div class="title">' + searcher[i].title + '</div>';
-        returnString+='<br>' + searcher[i].wordcount;
-        returnString+='<br> ' + searcher[i] .snippet + '</div>';
-        returnString+= '<div style="clear:both;"></div>';
 
+        //create page elements
+        let page = document.createElement("div");
+        let title = document.createElement("div");
+        let snippet = document.createElement("div");
+        let results = document.querySelector("#results");
+
+        // add attributes
+        title.innerHTML = searcher[i].title;
+        title.class = 'title';
+
+        snippet.innerHTML = searcher[i].snippet;
+
+        page.appendChild(title);
+        page.appendChild(snippet);
+
+        page.id = "article" + i;
+        page.className = "page";
+        page.style.cursor = "pointer";
+
+        page.addEventListener("click",()=>{
+          let url = "https://en.wikipedia.org/wiki/ " + title.innerHTML;
+
+          window.open(url,'_blank');
+
+
+        });
+
+        results.appendChild(page);
 
       }
-      returnString+='<br>';
-      //
-      // let returnString = '<div class="results"><h2></h2>Title: ' + title + '<br>';
-      // returnString += '<a target="_blank" href= ' + url + ' ><h2></h2>Link: ' + url + '</a></div>';
 
-      $('#results').append(returnString);
       $("#results").show();
-      // $('#results').show().append(returnString);
 
     })
     .error(function(){
       alert('request failed');
     });
 
-
-
-
   });
-
-
-
 
 })();
